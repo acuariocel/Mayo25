9dece8234d508bb704ec60b761890cc6c5548092
KRADAC <KRADAC@PROGRAMING-PC>
2014-06-01 10:26:02 -0500

    AGREGAR STRING ING DIEGO

--- JUNIO/Junio01/FuncionesString.cpp
+++ JUNIO/Junio01/FuncionesString.cpp
@@ -1,1 +1,9 @@
 #include <string>
+using namespace std;
+bool tamPalIg(string a, string b){
+    bool ig=false;
+    if(a.length()==b.length()){
+        ig=true;
+    }
+    return ig;
+}
---
+++ JUNIO/Junio01/TestString.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "FuncionesString.cpp"
+using namespace std;
+ 
+int main(){
+   cout<<tamPalIg("diego","cel"); 
+    return 0;
+}
