953b64e0425e765269fa8c14c4612721fd373f98
CEL <CEL@Internet-2>
2014-06-01 10:55:09 -0500

    Creación de Funciones string Alex

--- JUNIO/Junio01/TestString.cpp
+++ JUNIO/Junio01/TestString.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include "FuncionesString.cpp"
 using namespace std;
  
 int main(){
-   cout<<tamPalIg("diego","cel"); 
-   //cout<<strcmp("diego","diego"); ///0 si son iguales
-   //cout<<strlen("diego"); ///tamaño de una cadena
+    char *x="Alex jhj  hb hjjj",*y="jose";
+	char m[5]="Alex", n[5]="Alex";	
+    //cout<<tamPalIg("diego","cel"); 
+   //cout<<strcmp(x,y); ///0 si son iguales
+   cout<<strlen(x); ///tamaño de una cadena
     return 0;
 }
